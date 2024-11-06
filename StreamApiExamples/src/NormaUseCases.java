480c0569d8ed702e02fcaf61d62dfef6eb6e8abf
Chandani <er.chandni99@gmail.com>
2024-11-06 18:26:25 +0530

    logger added

--- StreamApiExamples/src/NormaUseCases.java
+++ StreamApiExamples/src/NormaUseCases.java
@@ -1,15 +1,19 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class NormaUseCases {
-
+    public static Logger logger = LogManager.getLogger(NormaUseCases.class.getName());
     public  void dataCollection(){
+        logger.info("Inside data collection method");
 //        Example of Number collection
         List<Integer> list = Arrays.asList(10,57,556,56,109, 20, 117);
         List<Integer> result = list.stream().map(elem->elem+10).collect(Collectors.toList());
-
+        logger.debug(list);
         System.out.println("mapped numbers with +10 : " +result);
 
 //        Example of string collection
@@ -21,7 +25,7 @@ public class NormaUseCases {
     }
 
     public void dataAggrigation(){
-
+        logger.info("Inside data aggrigation method method");
 //        example of Number collection
         List<Integer> list = Arrays.asList(10,57,556,56,109, 20, 117);
         int result = list.stream().reduce(0, (element1, element2) -> element1 + element2);
@@ -45,6 +49,7 @@ public class NormaUseCases {
 
     public void dataFiltering(){
 
+        logger.info("Inside data filtering method");
 //      example of Number collection
         List<Integer> list = Arrays.asList(10,57,556,56,109, 20, 117);
         List<Integer> result = list.stream().filter(element -> element % 2==0).collect(Collectors.toList());
@@ -61,6 +66,7 @@ public class NormaUseCases {
         List<String> longestString = words.stream()
                 .filter(word -> word.length() > 5 == true).collect(Collectors.toList());
 
+        logger.debug("The list of string before :"+words+"\n The list of Words after apply flter method : " +longestString);
         // Displaying the longest String
         System.out.println(longestString);
     }
